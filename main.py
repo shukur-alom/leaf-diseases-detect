5cade59d0d94305e97f36dba0c833d41e687829e
shukur-alom <shukuralom1234@gmail.com>
2024-01-13 21:23:25 +0600

    streamlit

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img,img_to_array
+import streamlit as st
+import cv2 as cv
 import numpy as np
+import tensorflow as tf
 
-
-leaf_deases_model = load_model('Training/Leaf_Deases (1).ipynb')
 
 label_name = ['Apple scab','Apple Black rot', 'Apple Cedar apple rust', 'Apple healthy', 'Cherry Powdery mildew',
 'Cherry healthy','Corn Cercospora leaf spot Gray leaf spot', 'Corn Common rust', 'Corn Northern Leaf Blight','Corn healthy', 
@@ -11,11 +10,21 @@ label_name = ['Apple scab','Apple Black rot', 'Apple Cedar apple rust', 'Apple h
 'Pepper bell healthy', 'Potato Early blight', 'Potato Late blight', 'Potato healthy', 'Strawberry Leaf scorch', 'Strawberry healthy',
 'Tomato Bacterial spot', 'Tomato Early blight', 'Tomato Late blight', 'Tomato Leaf Mold', 'Tomato Septoria leaf spot',
 'Tomato Spider mites', 'Tomato Target Spot', 'Tomato Yellow Leaf Curl Virus', 'Tomato mosaic virus', 'Tomato healthy']
-                   
 
-path = input('Imag Path')
-img = img_to_array(load_img(path,target_size=(150,150,3)))
+st.write("""The leaf disease detection model is built using deep learning techniques, and it uses transfer learning to leverage the pre-trained knowledge of a base model. The model is trained on a dataset containing images of 33 different types of leaf diseases. For more information about the architecture, dataset, and training process, please refer to the code and documentation provided.""")              
 
-pridict_image = leaf_deases_model.predict( img.reshape((1,) + img.shape ))
+st.write("Please input only leaf Images of Apple, Cherry, Corn, Grape, Peach, Pepper, Potato, Strawberry, and Tomato. Otherwise, the model will not work perfectly.")
 
-print(f"{label_name[np.argmax(pridict_image)]} {pridict_image[0][np.argmax(pridict_image)]*100}%")
+model = tf.keras.models.load_model('Training/model/Leaf Deases(96,88).h5')
+
+
+uploaded_file = st.file_uploader("Upload an image")
+if uploaded_file is not None:
+    image_bytes = uploaded_file.read()
+    img = cv.imdecode(np.frombuffer(image_bytes, dtype=np.uint8), cv.IMREAD_COLOR)
+    normalized_image = np.expand_dims(cv.resize(cv.cvtColor(img, cv.COLOR_BGR2RGB), (150, 150)), axis=0)
+    predictions = model.predict(normalized_image)
+    st.image(image_bytes)
+    if predictions[0][np.argmax(predictions)]*100 >= 80:
+        st.write(f"Result is : {label_name[np.argmax(predictions)]}")
+    else:st.write(f"Try Another Image")
